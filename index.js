811b8065a75c89ca3677785d8ea1b8ea60aa7ef0
Robbie Perrone <robert.perrone1@marist.edu>
2023-02-20 17:07:26 -0500

    Homepage
    This push is a more finalized version of the homepage.]
    
    I mostly finalized the style and formatting so that the nav bar works both on desktop and mobile. Also made sure that the footer does not behave differently than expected.
    
    The next push will include more content and images on the other pages.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+$(function(){
+    $("#nav-bar").load("nav.html");
+});
+
+$(function(){
+    $("#footer").load("footer.html");
+});
+
 function hamburgerNav() {
     var x = document.getElementById("topNav");
     if (x.className === "nav") {
@@ -7,34 +15,17 @@ function hamburgerNav() {
     }
 }
 
-$(function(){
-    $("#nav-bar").load("nav.html");
-});
-
-function footer() {
-    var x = document.getElementById("bottomFooter");
-    if (x.className === "footer") {
-        x.className += " responsive";
-    } else {
-        x.className = "footer";
-    }
-}
-
-$(function(){
-    $("#footer").load("footer.html");
-});
-
 // Get the container element
-var navContainer = document.getElementById("topNav");
-// Get all buttons with class="btn" inside the container
-var navItems = document.getElementsByClassName("page");
+// var navContainer = document.getElementById("topNav");
+// // Get all buttons with class="btn" inside the container
+// var navItems = document.getElementsByClassName("page");
 
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < navItems.length; i++) {
-    navItems[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        console.log(current);
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < navItems.length; i++) {
+//     navItems[i].addEventListener("click", function() {
+//         var current = document.getElementsByClassName("active");
+//         console.log(current);
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
